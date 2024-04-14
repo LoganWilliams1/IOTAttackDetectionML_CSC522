7e2b9465c0464a7872504e5c88da1d9d15fe8e9e
Logan Williams <loganwilliamslw1@gmail.com>
2024-04-13 22:29:58 -0400

    call to_numpy on y column of synthetic data

--- regression/regression_synthetic.py
+++ regression/regression_synthetic.py
@@ -13,25 +13,7 @@ train, test = util.import_dataset(7, "regression")
 del train
 
 
-# encoders_new = joblib.load('../data_breakdown/column_encoders.joblib')
-# for column in discrete_columns:
-#     if column in encoders_new:
-#         synthetic_dataset[column] = encoders_new[column].inverse_transform(synthetic_dataset[column])
-
-# y_train = synthetic_dataset[util.y_column]
-# y_test = test[util.y_column]
-
-# X_train = synthetic_dataset.drop(util.y_column, axis=1)
-# X_test = test.drop(util.y_column, axis=1)
-
-# label_encoder = LabelEncoder()
-# y_train_encoded = y_train.to_numpy()
-# y_test_encoded = label_encoder.fit_transform(y_test)
-
-# del test
-# num_classes = len(label_encoder.classes_)
-# print(" Number of classes is:" )
-# print(num_classes)
+train[util.y_column].to_numpy()
 
 
 y_pred, y_test = regression.train_test_logistic_regression(synthetic_dataset, test)
